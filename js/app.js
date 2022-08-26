9728920a8ebe26899124a695613a342e11ad0340
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-08-26 11:30:41 -0400

    Display palindrome message when isPalindrome()
    Display palindrome message when isPalindrome() is true

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 const userInputEl = document.querySelector('#maybe-palindrome');
+const successMessage = document.querySelector('#palindrome-message');
 
 userInputEl.addEventListener('input', function(event) {
 	const value = event.target.value;
+	if (isPalindrome(value)) {
+		successMessage.className = 'is-palindrome';
+	} else {
+		successMessage.className = '';
+	}
 	console.log(value);
 });
 
