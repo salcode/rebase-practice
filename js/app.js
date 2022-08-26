0fdfb02694379b88c8b2dd5d8f4630b02b6a4590
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-08-26 15:40:34 -0400

    Change JS tabs to spaces

--- js/app.js
+++ js/app.js
@@ -2,17 +2,17 @@ const userInputEl = document.querySelector('#maybe-palindrome');
 const successMessage = document.querySelector('#palindrome-message');
 
 userInputEl.addEventListener('input', function(event) {
-	const value = event.target.value;
-	if (isPalindrome(value)) {
-		successMessage.className = 'is-palindrome';
-	} else {
-		successMessage.className = '';
-	}
-	console.log(value);
+  const value = event.target.value;
+  if (isPalindrome(value)) {
+    successMessage.className = 'is-palindrome';
+  } else {
+    successMessage.className = '';
+  }
+  console.log(value);
 });
 
 function isPalindrome(value) {
-	return value === [...value].reverse().join('');
+  return value === [...value].reverse().join('');
 }
 
 function testIsPalindrome() {
