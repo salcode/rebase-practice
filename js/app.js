6de22acae0521ce82b14f7aa2d8f4dd88d0bca1c
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-08-26 10:50:39 -0400

    Add event listener for user input

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-console.log('this is app.js');
+const userInputEl = document.querySelector('#maybe-palindrome');
+
+userInputEl.addEventListener('input', function(event) {
+	const value = event.target.value;
+	console.log(value);
+});
